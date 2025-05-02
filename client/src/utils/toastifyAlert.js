3d036ca6522ae0f0250b59fd75f3962f1f057b7c
VikRic <vr222im@student.lnu.se>
2025-05-02 13:07:17 +0200

    Add .gitlab file

--- client/src/utils/toastifyAlert.js
+++ client/src/utils/toastifyAlert.js
@@ -3,7 +3,7 @@ import { toast } from 'react-toastify'
 export const showFailedAlert = (message) => {
   toast.error(message, {
     position: 'top-center',
-    autoClose: 2000,
+    autoClose: 5000,
     closeOnClick: true,
     draggable: true,
     theme: 'light',
