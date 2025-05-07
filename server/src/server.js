aaede27b14a3268ec4292659493a0c5bf81eec1d
VikRic <vr222im@student.lnu.se>
2025-05-07 11:42:29 +0200

    Add fallback port

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,6 @@ import { securityHeaders, limiter } from './middleWare/security.js'
 
 try {
   dotenv.config()
-  console.log(process.env.DB_CONNECTION_STRING)
   // Connect to MongoDB.
   await connectToDatabase(process.env.DB_CONNECTION_STRING)
 
@@ -50,7 +49,8 @@ try {
   /*   app.use(errorHandler) */
 
   // Start server
-  const server = app.listen(process.env.PORT, () => {
+  const PORT = process.env.PORT || 5000
+  const server = app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${server.address().port}`)
     console.log('Press Ctrl-C to terminate...')
   })
