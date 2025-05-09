c857f5855bbdab417c7c0f481de089c90665c94d
VikRic <vr222im@student.lnu.se>
2025-05-09 10:14:25 +0200

    change port

--- server/src/server.js
+++ server/src/server.js
@@ -49,7 +49,7 @@ try {
   /*   app.use(errorHandler) */
 
   // Start server
-  const PORT = process.env.PORT || 5000
+  const PORT = process.env.PORT || 8080
   const server = app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${server.address().port}`)
     console.log('Press Ctrl-C to terminate...')
