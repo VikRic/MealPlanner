94094afa1567f879b564f4642bb208fdbe39f69d
VikRic <vr222im@student.lnu.se>
2025-05-19 23:22:30 +0200

    Add heartbeat for mongoDB

--- server/src/util/cron.js
+++ server/src/util/cron.js
@@ -1,8 +1,19 @@
 import cron from 'node-cron'
 import { RecipeController } from '../controllers/RecipeController.js'
+import { RecipeModel } from '../models/RecipeModel.js'
 // This fetches 2 recipes every 1 hour.
 cron.schedule('0 * * * *', async () => {
   const controller = new RecipeController()
   await controller.getReq(2)
   console.log('Fetching recipes')
+})
+
+cron.schedule('*/10 * * * *', async () => {
+  try {
+    await RecipeModel.findOne()
+
+    console.log('Sending heartbeat recipes')
+  } catch {
+    console.error('Heartbeat failed')
+  }
 })
