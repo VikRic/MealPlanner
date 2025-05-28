a72424d191c536cae0e9186815d4bea615999a57
VikRic <vr222im@student.lnu.se>
2025-05-28 14:48:05 +0200

    Revert change and add console.log

--- server/src/middleWare/requireAuth.js
+++ server/src/middleWare/requireAuth.js
@@ -14,6 +14,7 @@ export function requireAuth (req, res, next) {
   if (!userId) {
     return res.status(401).json({ error: 'Not logged in' })
   }
+  console.log(userId)
 
   req.userId = userId
   next()
