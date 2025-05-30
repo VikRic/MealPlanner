8233fa2b0503337480dbbf51730ecdf1671f70b2
VikRic <vr222im@student.lnu.se>
2025-05-30 13:53:17 +0200

    Try userButton

--- client/src/reactComponents/layout/header/Header.jsx
+++ client/src/reactComponents/layout/header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { useUser, SignOutButton } from '@clerk/clerk-react'
+import { useUser, SignOutButton, UserButton } from '@clerk/clerk-react'
 import './header.css'
 
 function Header() {
@@ -16,9 +16,10 @@ function Header() {
             {isLoaded && user ? (
               <li className='nav-auth'>
 
-                <SignOutButton>
+              <UserButton></UserButton>
+{/*                 <SignOutButton>
                   <button style={{padding: '10px'}} className="logout-btn">{user.username}</button>
-                </SignOutButton>
+                </SignOutButton> */}
 
               </li>
             ) : (
