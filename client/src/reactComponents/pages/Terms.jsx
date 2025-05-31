cd3b48eeba23590732bdbb1a8a3b5265380e710c
VikRic <vr222im@student.lnu.se>
2025-05-31 23:28:09 +0200

    Fix incorrect h2 text

--- client/src/reactComponents/pages/Terms.jsx
+++ client/src/reactComponents/pages/Terms.jsx
@@ -3,7 +3,7 @@ function Terms() {
     <div className="terms" style={{ padding: '10px'}}>
    <h1> Disclaimer - Educational Use Only</h1>
 
-      <h2>This website was created as a part of a non-commercial academic assignment and is intended solely for educational purposes. It may include data or funcionality powered by the Spoonacular API. Enter how many servings per recipe.</h2>
+      <h2>This website was created as a part of a non-commercial academic assignment and is intended solely for educational purposes. It may include data or funcionality powered by the Spoonacular API.</h2>
        <br />
 
       <h2>I do not claim ownership of any third-party content or API-used. The use of the Spoonacular API is for demonstration purposes only, not for commercial use or distribution.</h2>
